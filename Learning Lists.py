c32ed65ef884c694b7718c4638d71f8a77b7e210
Jayaprakash <prakashjj26@gmail.com>
2021-05-30 18:17:48 +0530

    Added Matrix Codes

--- Learning Lists.py
+++ Learning Lists.py
@@ -1,3 +1,9 @@
 employeeLists = ["Jayaprakash",'JayaKumar','Deavki','Jayabharathi']
 for employeeList in employeeLists:
     print("your Employee Name is: "+employeeList)
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for m in matrix:
+    print("Matrix Values "+ str(m))
+    for i in m:
+        print("Matrix Values are "+ str(i))
